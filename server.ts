beafd10cc4d7359f3b530b33f986d141def448b6
M0hammedK <mmm2016alkaf@gmail.com>
2025-02-09 19:09:31 +0300

    c

--- server.ts
+++ server.ts
@@ -1,10 +1,13 @@
 import * as jsonserver from "json-server";
+import * as cors from'cors'
 
-const server = jsonserver.create();
+
+const server =  jsonserver.create();
 const router = jsonserver.router("db.json");
 const middleware = jsonserver.defaults();
 const port = process.env.PORT || 4000;
 
+server.use(cors({origin: 'https://tec-task.vercel.app', credentials:true}))
 server.use(middleware);
 server.use(router);
 server.listen(port, () => {
