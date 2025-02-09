20237b61a7ce4c02565583fb3643943af48c4eb5
M0hammedK <mmm2016alkaf@gmail.com>
2025-02-09 23:21:05 +0300

    .

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server')
+
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middleware = jsonServer.defaults({noCors:true});
+const port = process.env.PORT || 4000;
+
+
+server.use(middleware); // Add default middleware (json-server)
+server.use(router); // Add routes from db.json
+
+server.listen(port, () => {
+  console.log("server running on port " + port);
+});
