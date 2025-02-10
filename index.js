5061b3cba3e67ce4bbc6a36017709a17a0a30621
M0hammedK <mmm2016alkaf@gmail.com>
2025-02-10 09:33:23 +0300

    3

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 
 const jsonServre = require("json-server");
+const cors = require("cors");
 
 const server = jsonServre.create();
 const router = jsonServre.router("./db.json");
 const middleware = jsonServre.defaults({noCors:true});
 
-server.use(router);
+server.use(cors())
 server.use(middleware);
+server.use(jsonServre.bodyParser);
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow any frontend
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+server.use(router);
 
 server.listen(4000, () => {
   console.log("server listen to port 4000");
